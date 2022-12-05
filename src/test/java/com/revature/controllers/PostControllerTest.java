560ebcc4f72b3a08b15378ed0f9ef5c6eebfc9c0
Rxcthefirst <81718883+Rxcthefirst@users.noreply.github.com>
2022-12-05 04:01:47 -0800

    created files for testing and started post repository tests git status

--- src/test/java/com/revature/controllers/PostControllerTest.java
+++ src/test/java/com/revature/controllers/PostControllerTest.java
@@ -51,8 +51,7 @@ class PostControllerTest {
 
     @Test
     void testUpsertPost() {
-        Post post = new Post();
-        post.setText("content");
+        Post post = new Post(1,"text", "text", null, null);
         ResponseEntity<Post> responseEntity = new ResponseEntity<>(post, HttpStatus.OK);
         restTemplate.postForEntity(baseUrl, responseEntity, Post.class);
     }
