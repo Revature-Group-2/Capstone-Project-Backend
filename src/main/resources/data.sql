cf2e780c1ed3f8c4154faf9da06a16427ad61273
Rxcthefirst <81718883+Rxcthefirst@users.noreply.github.com>
2022-12-07 13:08:06 -0800

    first commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,22 +14,16 @@ INSERT INTO users (id, email, password, first_name, last_name) VALUES (
 );
 
 INSERT INTO posts (id, text, image_url,vote_count, author_id) VALUES (
-    10000,
+    DEFAULT,
     'The classic',
     'https://i.imgur.com/fhgzVEt.jpeg',
     0,
     1
 ),
 (
-    10001,
+    DEFAULT,
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     '',
-    0,
-    1
-), (
-    DEFAULT,
-    'The classic',
-    'https://i.imgur.com/fhgzVEt.jpeg',
     0,
     1
 );
