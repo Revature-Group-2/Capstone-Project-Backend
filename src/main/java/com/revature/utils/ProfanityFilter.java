6426424ed76b365eaad44ff69dbe467d3f14aa42
HyungJoo Yoon <hyungjooyoon@disroot.org>
2022-12-14 08:34:39 -0800

    fixed file path

--- src/main/java/com/revature/utils/ProfanityFilter.java
+++ src/main/java/com/revature/utils/ProfanityFilter.java
@@ -1,8 +1,11 @@
 package com.revature.utils;
 
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,9 +18,15 @@ public class ProfanityFilter {
 
         Set<String> whitelist = new HashSet<>();
         Set<String> blacklist = new HashSet<>();
+
+        Resource resource1 = new ClassPathResource("blacklist.txt");
+        Resource resource2 = new ClassPathResource("whitelist.txt");
+        File file1 = resource1.getFile();
+        File file2 = resource2.getFile();
+
         try {
-            blacklist = new HashSet<>(Files.readAllLines(Path.of("src/main/resources/blacklist.txt")));
-            whitelist = new HashSet<>(Files.readAllLines(Path.of("src/main/resources/whitelist.txt")));
+            blacklist = new HashSet<>(Files.readAllLines(Path.of(file1.getAbsolutePath())));
+            whitelist = new HashSet<>(Files.readAllLines(Path.of(file2.getAbsolutePath())));
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
